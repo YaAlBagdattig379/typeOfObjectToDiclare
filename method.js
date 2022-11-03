25dd9075085713910b0927d97b68793b5426a6c5
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-03 14:59:22 +0530

    added

--- method.js
+++ method.js
@@ -5,3 +5,23 @@ method.
 >>> jehetu eta function tai eta ke return  ,parameter, call kora, ebong ei object 
 er j kno property access kora and change kora jai ei function er dara.    
 */ 
+// egp: 1 
+const studdent = {
+    name: 'Hero Alam',
+    isRich: false,
+    roll: 24,
+    major: 'english',
+    subjects:['bengali','mathematics','history','English'],
+    bestFriend:{
+        name : 'keltu',
+        isRich: true,
+        major: 'english',
+        roll: 22,
+        subjects:['geography','history','Phsical Science']
+    },
+    sport:'playCricket',
+    takeExam: function(){
+        console.log(this.name)
+    }
+};
+console.log(studdent);
