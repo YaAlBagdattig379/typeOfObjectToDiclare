e20c3451d465b6cda4622525ed7e36bae46f468e
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-03 16:31:46 +0530

    added

--- method.js
+++ method.js
@@ -1,13 +1,15 @@
 /*
-what is method  in js ?
+what is method  in j
+s ?
 >>> jokhon object er vitor kono ekta propertir value/man function lekha  jai etai
 method.
 >>> jehetu eta function tai eta ke return  ,parameter, call kora, ebong ei object 
 er j kno property access kora and change kora jai ei function er dara.    
 */ 
 // egp: 1 
-const studdent = {
-    name: 'Hero Alam',
+const student = {
+    name: 'Hero Alam',      
+    money:5000,
     isRich: false,
     roll: 24,
     major: 'english',
@@ -21,7 +23,22 @@ const studdent = {
     },
     sport:'playCricket',
     takeExam: function(){
-        console.log(this.name)
+        console.log(this.name ,"boka jamami")
+    },
+    treatDay: function(expense,tips){
+        this.money = this.money - expense - tips;
+        return this.money;
+        // console.log(this.money,'valo chalak jamai ')
     }
+   
 };
-console.log(studdent);
+// student.takeExam()
+// const remaining1 = student.treatDay(1000,100)
+const remaining2 = student.treatDay(500,200)
+// try{
+    // console.log(remaining1);
+    console.log(remaining2);''
+// }catch(er){
+//     console.log(er)
+// }
+// ;
