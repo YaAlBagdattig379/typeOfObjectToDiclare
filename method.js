34a0c295490f4d1938114b9015dc2291618f6dbb
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-03 14:49:55 +0530

    added

---
+++ method.js
@@ -0,0 +1,7 @@
+/*
+what is method  in js ?
+>>> jokhon object er vitor kono ekta propertir value/man function lekha  jai etai
+method.
+>>> jehetu eta function tai eta ke return  ,parameter, call kora, ebong ei object 
+er j kno property access kora and change kora jai ei function er dara.    
+*/ 
