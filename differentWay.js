f3db88a08a45097e51664bb3037fe2f90d6d1410
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-03 12:28:41 +0530

    added

---
+++ differentWay.js
@@ -0,0 +1,11 @@
+// different way to diclare Object in JavaScript 
+
+//method:- 1.....using object literal
+const student = {name:'Here Alam',age: 20};
+// console.log(student); //op>> { name: 'Here Alam', age: 20 }
+
+//method:- 2.....using constructor
+const person = new Object();
+// console.log(person); // op >>>  {}
+
+//method:- 3.....variable er jaigai 'inheretence/null'dite hobe faka rakle hobe na
