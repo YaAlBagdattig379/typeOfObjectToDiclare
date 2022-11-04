d91f146626074c3d16fb993ddbaf6f4bd525a743
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-04 10:06:38 +0530

    added

---
+++ loop-object.js
@@ -0,0 +1,25 @@
+//********** loop in Object ***************
+/**
+ ******** rule of loop which one is more advantage for specific purposes ********
+
+
+ 
+ 1..name:- 'regular for loop'>>> 
+       for(const i = 0 ; i > 10 ; i++){}
+       usage: >>> normally use kora hoi ei ti .
+
+
+2..name:- 'for of' >>>>
+         for(const number of students){}
+         usage:> ei loop ti specially for array er upor use hoi.
+
+3..name:- 'for in' >>>>
+         for(const per in students){}
+         usage:> ei loop ti specially for object er upor use hoi.
+*/
+const bottle = {
+    name: 'water-bottle',
+    price:  50,
+    material: 'plustics',
+    weight: '20grms'
+};
