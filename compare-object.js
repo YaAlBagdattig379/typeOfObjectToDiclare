5e17c12f82ca3ab647bf53cb036594285dc4b352
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-04 16:23:12 +0530

    added

--- compare-object.js
+++ compare-object.js
@@ -1,9 +1,48 @@
-// examples 
+/**
+ * *********************rules to compare with object****************
+ * 
+ * 
+ * ***1...object k ar ekti object er sathe compare korar somoi upor upor        conmapre  korle same / true hobe na jodio tader value ekoi thakuk..
+ * 
+ ****2...tobe ha jodi stringify kore compare kore tahole true / same hobe..
+
+
+ ****3...jodi ek objeact er value ar ekti object er sathe ankta dewa hooi tahole true hobe but tader property kom ba beshi korle true hobe na..
+ *
+ *
+ */
+//..1..for examples according to rules
 const first = {a: 1};
 const second = {a: 1};
-if(first == second){
+if(first === second){
     console.log('object are equal !')
 }
 else{
+    console.log('object are not same  !') //op >>> object are not same  !
+}          //NB:> sob value  ek thakar sotteo false holo becoz etai rule
+
+
+
+//..2..for examples according to rules
+const first2 = {a: 1};
+const second2 = {a: 1};
+if(JSON.stringify(first) === JSON.stringify(second)){
+    console.log('object are equal !') //op >>> object are same  !
+                        
+}      //NB:> becoz eta stringify kore  compare kora hoache.                        
+else{
     console.log('object are not same  !')
-}
+}        
+
+
+//..3..for examples according to rules
+const first3 = {a: 1};
+const second3 = {a: 1};
+const third3 = second3;
+if(JSON.stringify(first3) ===JSON.stringify(third3)){
+    console.log('object are equal !') //op >>> object are same  !
+                        
+}      //NB:> becoz eta stringify kore  just tader variable name gula sudhu alada kora  hoache  compare kora hoache.                        
+else{
+    console.log('object are not same  !')
+}        
